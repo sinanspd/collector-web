c7b14e54ddf4fcc4156c24e5912e0df4fffbc979
sinanspd <sinanspd@gmail.com>
2018-12-11 23:45:43 -0500

    upto 4K support

--- src/app/band/band.component.ts
+++ src/app/band/band.component.ts
@@ -70,7 +70,12 @@ export const MEMBERS = [
 		instruments3: "Other: Gibraltar Hardware, Pearl 2002 double bass pedal",
 		instruments4: "",
 		instruments5: "Home Studio: -",
-		biolong: "Coming Soon"
+		biolong: "In his own words: 'Hey there! Terrific intro I know... I play the drums in the band & don't do much else. I currently" +
+		" reside in Istanbul because I am going through Medical School (no, really). I'm close to graduation (again - no, really)." + 
+		" I have been behind the kit for about 10 years and have a variety of influences from the grat John Bonham and the talented " +
+		"Chris Adler, and lately Brann Dailor - seriously how does he sign and play? Metal will always be special to me but as an artist" +
+		" I believe in extending my repertoire. Among aother genres, I listen to Jazz, Blues and Rock. I won't list them all here" +
+		"because there are A LOT of genres that somewhat have a 'taboo' status in the metal scene. More about me in the summary tab...'"
     }
 ]
 
--- src/app/news/cached-news.ts
+++ src/app/news/cached-news.ts
@@ -58,5 +58,40 @@ export const CachedNews : News[] = [
 		date: "04/19/18 12:04 PM",
 		summary: "Mighty Metal Injection has honored us by premiering our debut single End This Life ft. Warrel Dane and Chris Broderick",
 		body: ""
+	},
+	{
+		id: "9",
+		title: "RIP Warrel Dane",
+		date: "12/15/17 12:04 PM",
+		summary: "As of this morning, our dear friend Warrel has left this mortal world. Read Sinan's statement regarding Warrel Dane's unexpected demise here",
+		body: ""
+	},
+	{
+		id: "10",
+		title: "We welcome Warrel Dane to Liberation's Fall",
+		date: "07/06/17 01:13 PM",
+		summary: "The mighty Warrel Dane of Nevermore and Sanctuary is the latest and final name to guest on Liberation's Fall",
+		body: ""
+	},
+	{
+		id: "11",
+		title: "Vicky Psarakis to appear on Liberation's Fall",
+		date: "03/10/17 07:37 PM",
+		summary: "Vicky Psarakis of The Agonist becomes the second guest to appear on Liberation's Fall",
+		body: ""
+	},
+	{
+		id: "12",
+		title: "All For One, One For All",
+		date: "02/24/17 04:21 PM",
+		summary: "To celebrate our upcoming collabration with Chris Broderick we are giving away a bunch of his picks!",
+		body: ""
+	},
+	{
+		id: "13",
+		title: "Chris Broderick joins Liberation's Fall",
+		date: "12/13/16 12:44 AM",
+		summary: "We are happy to announce that Chris Broderick will honor us by performing a guest solo on our debut album",
+		body: ""
 	}
 ]
